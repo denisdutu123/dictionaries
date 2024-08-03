5c6f86d6571a00acdb0365cc4c8e20c8ba1ca2d6
denisdutu123 <dutudenis42@gmail.com>
2024-08-03 13:57:42 +0100

    Add files via upload
    basics of dictionaries

---
+++ test project.py
@@ -0,0 +1,16 @@
+di1={"Bye":9,"True":0.9,100:"You"}
+print(di1)
+print(len(di1))
+print(di1[100])
+#dictionary for a person
+per={"name":"denis","surname":"dutu","age":15,"height":"5'9","name":"bye"}
+#accessing values
+print(per["name"])
+print(per["surname"])
+print(per["age"])
+print(per["height"])
+#adding a value
+per["p"]="road"
+print(per)
+per["age"]=1
+print(per)

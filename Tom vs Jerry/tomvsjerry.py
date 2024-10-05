4bbbf2fba4b51e8a7c63afa0dafe51923f4b76a0
denisdutu123 <dutudenis42@gmail.com>
2024-10-05 17:09:52 +0100

    Update tomvsjerry.py

--- Tom vs Jerry/tomvsjerry.py
+++ Tom vs Jerry/tomvsjerry.py
@@ -7,17 +7,24 @@ tom = Actor("tom.png")
 jerry = Actor("jerry.png")
 tom.pos = 500,325
 jerry.pos = 600,275
+gov=False
 scor = 0
 
 def draw():
     screen.blit("ground.jpg",(0,0))
     tom.draw()
     jerry.draw()
+    if gov:
+         screen.fill("Dark green")
+         screen.draw.text("GAME OVER,score:"+str(scor),center = (500,350),fontsize = 50)
 
 
 def position():
         jerry.pos = random.randint(0,1000), random.randint(0,600)
 
+def tim():
+     global gov
+     gov = True
 
 def update():
       global scor
@@ -31,6 +38,9 @@ def update():
           tom.y+=7
       if tom.colliderect(jerry):
            position()
+           scor+=1
+
+clock.schedule(tim,60)
       
 
 

25b1aca85e779262ba5dc1978000f0c7f057c777
denisdutu123 <dutudenis42@gmail.com>
2024-12-07 17:31:53 +0000

    Update shooting game.py

--- shooting game/shooting game.py
+++ shooting game/shooting game.py
@@ -13,6 +13,7 @@ ene.append(Actor("enemy.png"))
 ene[-1].x = 50
 ene[-1].y = -10
 score = 0
+ove = False
 def on_key_down(key):
     if key == keys.SPACE:
        bul.append(Actor("bullets.png"))
@@ -25,19 +26,28 @@ def draw():
       i.draw()
     for o in bul:
       o.draw()
+    if ove:
+        screen.fill("Dark red")
+        screen.draw.text("Game Over,your score is:"+str(score),fontsize =35,center=(500,325) )    
 
+def over():
+    global ove
+    ove = True
 
+clock.schedule(over,10)
+    
 
 
 def update():
+    global score
     pass
     #key movements
     if keyboard.left:
-        shooter.x-=5
+        shooter.x-=9
         if shooter.x<0:
             shooter.x = 0
     if keyboard.right:
-        shooter.x+=5
+        shooter.x+=9
         if shooter.x>1000:
             shooter.x = 1000  
     for i in ene:
@@ -45,8 +55,16 @@ def update():
         if i.y > 650:
             i.y = -10
             i.x = random.randint (0,1000)
+        for c in bul:
+            if i.colliderect(c):
+                i.y=-10
+                i.x= random.randint (0,1000)
+                bul.remove(c)
+                score+=1
     for v in bul:
         v.y -= 10
+
+
 
 
 
